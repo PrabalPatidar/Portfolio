a3023b72ecaefabe8aad5a31adf676eea12db42b
PrabalPatidar <prabalpatelpip8@gmail.com>
2025-07-29 13:56:18 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,285 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import technoLogo from './assets/company_logo/techno_logo.jpeg';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import iistLogo from './assets/education_logo/iist_logo.png';
+import mpsLogo from './assets/education_logo/mps_logo.png'; 
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import respLogo from './assets/work_logo/responsice-img.png';
+import Password_generatorLogo from './assets/work_logo/password_gen_img.png';
+import bgChangeLogo from './assets/work_logo/bg_changer_img.png';
+import todoLogo from './assets/work_logo/todo_img.png';
+import bookstoreLogo from './assets/work_logo/bookstore_img.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      //{ name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      //{ name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      //{ name: 'Firebase', logo: firebaseLogo },
+      //{ name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      // { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      //{ name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      //{ name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: technoLogo,
+      role: "Web Developer",
+      company: "TechnoHacks Solutions",
+      date: "October 2024 - November 2024",
+      desc: " I have built several beginner-friendly web projects, including a Task Calculator, Login Page, Registration Page, and a News Website. The Task Calculator handles basic operations with a simple, responsive UI. The Login and Registration pages feature input validation and simulate user authentication. The News Website displays trending articles with a clean, modern layout. These projects helped me enhance my skills in HTML, CSS, and JavaScript, and provided a strong base for future development.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "Tailwind CSS",
+      ],
+    },
+    // {
+    //   id: 1,
+    //   //img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    // {
+    //   id: 2,
+    //   img: newtonschoolLogo,
+    //   role: "Frontend Intern",
+    //   company: "Newton School",
+    //   date: "September 2021 - August 2022",
+    //   desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "Javascript",
+    //     "Bootstrap",
+    //     "Figma",
+    //     "Material UI",
+    //   ],
+    // },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: iistLogo,
+      school: "Indore Institute of Science & Technology, Indore",
+      date: "Sept 2021 - July 2025",
+      grade: "7.30 CGPA",
+      desc: "I have completed my Bachelor's degree (B.Tech) in Computer Science from IIST College, Indore. During my time at IIST, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Web Development, and Software Engineering. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at IIST College has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelor Of Technology - B.Tech (Computer Science)",
+    },
+    {
+      id: 1,
+      img: mpsLogo,
+      school: "M P Public Higher Secondary School, Mandsaur",
+      date: "Apr 2020 - March 2021",
+      grade: "82%",
+      desc: "I completed my class 12 education from M P Public School, Mandsaur, under the M P board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "M P Board (XII) - PCM with Computer Science",
+    },
+    {
+      id: 2,
+      img: mpsLogo,
+      school: "M P Public Higher Secondary School, Mandsaur",
+      date: "Apr 2018 - March 2019",
+      grade: "81%",
+      desc: "I completed my class 10 education from M P Public School, Mandsaur, under the M P board, where I studied All subjects.",
+      degree: "M P Board (X)",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "GitHub Profile Detective",
+      description:
+        "A powerful and user-friendly React.js application designed to uncover and showcase detailed GitHub profile information. Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+      image: githubdetLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+      github: "https://github.com/PrabalPatidar/GithubProfileSearch",
+      webapp: "https://github-profile-search-rose.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "Usability Hub Clone",
+      description:
+        "I created a responsive website that works seamlessly on both mobile and laptop screens. Using HTML, CSS, and media queries, the layout adapts to different screen sizes, ensuring a smooth user experience. The design remains clean, accessible, and user-friendly across all devices, enhancing usability and visual appeal..",
+      image: respLogo,
+      tags: [ "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/PrabalPatidar/responsive-web",
+      webapp: "https://vermillion-gumption-38097f.netlify.app/",
+    },
+    {
+      id: 2,
+      title: "Password Generator",
+      description:
+        "This is a simple and secure password generator website that allows users to create strong, random passwords. Users can customize the password length and include numbers, special characters, and letters. It's designed to enhance online security by generating unpredictable passwords, making it ideal for personal or professional use. Fast, user-friendly, and effective!.",
+      image: Password_generatorLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/PrabalPatidar/password_generator",
+      webapp: "https://password-generator-green-sigma-12.vercel.app/",
+    },
+    {
+      id: 3,
+      title: "Todo App",
+      description:
+        "This Todo web app is a full-featured task management tool with complete CRUD functionality. Users can easily add, edit, and delete tasks in a clean and responsive interface. Designed for productivity and ease of use, it helps users stay organized and manage daily tasks efficiently. A perfect tool for personal or professional task tracking.",
+      image: todoLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/PrabalPatidar/Todo",
+      webapp: "https://todo-murex-two.vercel.app/",
+    },
+    {
+      id: 4,
+      title: "Background Changer",
+      description:
+        "This interactive website features a vibrant footer with 7–8 colorful buttons. Each button represents a different color, and clicking on any of them instantly changes the background color of the entire page. It's a fun, visually engaging UI project that demonstrates DOM manipulation and user interaction using HTML, CSS, and JavaScript.",
+      image: bgChangeLogo,
+      tags: ["React JS", "API", "Redux", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/PrabalPatidar/BgChanger",
+      webapp: "https://bg-changer-b81f.vercel.app/",
+    },
+    {
+      id: 5,
+      title: "BookStore website",
+      description:
+        "The Bookstore is a Full-Stack MERN project featuring user authentication with secure password storage in MongoDB. It includes a dark mode for enhanced user experience. Users can sign up or log in to access personalized features. Built using MongoDB, Express.js, React.js, and Node.js, the application offers a modern and secure interface for managing bookstore data efficiently.",
+      image: bookstoreLogo,
+      tags: ["HTML", "CSS", "JavaScript", "React JS", "Node.js", "MongoDB", "Express", "Tailwind CSS"],
+      github: "https://github.com/PrabalPatidar/BookStore",
+      webapp: "https://github.com/PrabalPatidar/BookStore",
+    },
+    // {
+    //   id: 6,
+    //   title: "Coding Master",
+    //   description:
+    //     "An ed-tech platform where users can access tech and coding-related blogs, notes, interview questions, e-books, and premium content with payment integration. Built with full-stack technologies for a seamless learning experience.",
+    //   image: cmLogo,
+    //   tags: ["React JS", "Node.js", "MongoDB", "Express", "Payment Integration"],
+    //   github: "https://codingmasterweb.in/",
+    //   webapp: "https://codingmasterweb.in/",
+    // },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
